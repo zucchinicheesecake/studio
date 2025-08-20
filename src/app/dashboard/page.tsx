f76ef5be891ce1de8ea2355dbad4e8220594936c
Amethyst Lænsi <amethystlansi@gmail.com>
2025-08-20 12:04:57 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -34,6 +34,7 @@ const GitHubIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5.5.09.68-.22.68-.48v-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.91-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.89 1.52 2.34 1.08 2.91.83.09-.65.35-1.08.63-1.33-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.29 2.75-1.02 2.75-1.02.53 1.2.18 2.31.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85v2.72c0 .27.18.58.69.48A10 10 0 0 0 22 12A10 10 0 0 0 12 2Z"/></svg>
 );
 
+// Moved AuthForm to be a standalone component within the file.
 const AuthForm = ({ type, onSubmit, onOAuth, email, setEmail, password, setPassword, error }: any) => (
     <Card>
         <CardHeader>
